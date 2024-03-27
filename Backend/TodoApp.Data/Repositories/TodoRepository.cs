6ec9bc975e8a7e1a3876fc58ab4cfe16db9dc6f8
DESKTOP-JNJJRTP\ahmed <ahmedshehzad786@gmail.com>
2024-03-27 23:44:07 +0100

    adding code comments - WIP

--- Backend/TodoApp.Data/Repositories/TodoRepository.cs
+++ Backend/TodoApp.Data/Repositories/TodoRepository.cs
@@ -9,9 +9,19 @@ namespace TodoApp.Data.Repositories
         public TodoRepository(TodoDbContext dbContext) : base(dbContext)
         {
         }
-        public async Task<Todo?> GetByIdAsync(Guid id, bool enableTracking = false, CancellationToken cancellationToken = default)
+
+        /// <summary>
+        /// Gets a Todo entity with the specified ID from the data store.
+        /// </summary>
+        /// <param name="id">The ID of the Todo entity to retrieve.</param>
+        /// <param name="enableTracking">A value indicating whether to enable change tracking for the query.</param>
+        /// <param name="cancellationToken">A cancellation token that can be used to cancel the operation.</param>
+        /// <returns>A task that represents the asynchronous operation, containing the retrieved Todo entity or null if no entity was found.</returns>
+        public async Task<Todo?> GetByIdAsync(Guid id, bool enableTracking = false,
+            CancellationToken cancellationToken = default)
         {
-            return await GetAsync(e => e.Id == id, enableTracking: enableTracking, cancellationToken: cancellationToken);
+            return await GetAsync(e => e.Id == id, enableTracking: enableTracking,
+                cancellationToken: cancellationToken);
         }
     }
 }
